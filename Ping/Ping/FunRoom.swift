875f893b149e2071113296fc571e104728d2385d
Ken Lee <klee229@ucr.edu>
2022-06-04 13:30:09 -0700

    Added Menu for Aggregation

--- Ping/Ping/FunRoom.swift
+++ Ping/Ping/FunRoom.swift
@@ -11,20 +11,20 @@ import AudioToolbox
 struct FunRoom: View {
     
     @State var showView: Bool = false
-    @State private var theMessage = ""
-    @State private var theDate = Date()
-    @State private var anonPoll = false
-    @State private var pinBoard = false
-    @State private var guides = false
-    @State private var theRating = 1
+    // @State private var theMessage = ""
+    //  @State private var theDate = Date()
+    // @State private var anonPoll = false
+    // @State private var pinBoard = false
+    // @State private var guides = false
+    // @State private var theRating = 1
     // var width = UIScreen.main.bounds.width
     
     var body: some View {
         ZStack(alignment: .bottom){
             LinearGradient(gradient: Gradient(colors: [.blue, .red, .green]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
-            VStack{
-                Spacer()
+            VStack(spacing: 30){
+              
                 
                 Button{
                     showView.toggle()
@@ -39,14 +39,67 @@ struct FunRoom: View {
                             .font(.system(size: 30, weight: . bold))
                     }
                 }
+                Spacer()
+                Spacer()
+            
+                
+                Menu {
+                    
+                 
+                    Link(destination: URL(string: "https://www.youtube.com/watch?v=dQw4w9WgXcQ")!, label: {
+                        Label(
+                            title: { Text("Add")},
+                            icon: { Image(systemName: "plus")}
+                        )
+                    })
+                  
+                    
+                    Link(destination: URL(string: "https://www.google.com/gmail/about/#")!, label: {
+                        Label(
+                            title: { Text("Email")},
+                            icon: { Image(systemName: "envelope")}
+                        )
+                    })
+            
+                    
+                    Link(destination: URL(string: "https://messages.google.com")!, label: {
+                        Label(
+                            title: { Text("Text")},
+                            icon: { Image(systemName: "bubble.left")}
+                        )
+                    })
+                    
+                    
+                    Link(destination: URL(string: "https://slack.com")!, label: {
+                        Label(
+                            title: { Text("Slack")},
+                            icon: { Image(systemName: "dpad")}
+                        )
+                    })
+                    
+                } label: {
+                
+                    Button(action: {}, label: {
+                     
+                        Text("Launch Apps")
+                            .bold()
+                            .font(.title2)
+                            .frame(width: 200, height: 50)
+                            .background(Color(.blue))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                        
+                    })
+                }
                 
                 Spacer()
+                Spacer()
                 
-              
+                
                 
                 
             }
-      
+            
             
             if showView{
                 
